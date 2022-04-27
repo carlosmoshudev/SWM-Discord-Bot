c0c84e97706504a76dc888996b7d0a94579cd20a
carlosmoshudev <carlos@carlosmoshu.com>
2022-04-27 03:12:16 +0200

    Update index.js
    Añadir función de Embed básico y listado hardcodeado de normas.

--- src/index.js
+++ src/index.js
@@ -18,6 +18,15 @@ function Sleep(milliseconds) {
     while (currentDate - date < milliseconds);
 }
 
+function BasicEmbedMessage(text, color) {
+    return {
+        embed: {
+            color: color,
+            description: text
+        }
+    };
+}
+
 function TypeRules(messageReference) {
     const welcomeChannel = messageReference.guild.channels.cache.find(
         channel => channel.id === chWelcome
@@ -40,10 +49,37 @@ function TypeRules(messageReference) {
     const firstColor    = '#FF0000';
     const secondColor   = '#C48B10';
     const thirdColor    = '#099CB0';
-
+    const normas = [
+        `1. Se amable con los demás. Se puede debatir de cualquier tipo de tema, incluso off-topic, sin necesidad de atacar u ofender a nadie.`,
+        `2. Evita temas conflictivos o delicados, estamos abiertos a todo tipo de temáticas e ideas y respetamos a las personas.`,
+        `3. No se permite contenido NSFW. El contenido sexual y el contenido violento no tienen espacio aquí.`,
+        `4. No abuses de las menciones masivas como @here, @everyone o roles; ni las uses para hacer spam.`,
+        `5. No publiques spam ni promociones en cualquier canal, ${spamChannel} es un canal dedicado a ello. Úsalo.`,
+        `6. El orden da paz, cada canal tiene su finalidad, no escribas cualquier cosa en cualquier canal.`,
+        `7. Recuerda que, como en todo servidor de comunidad, tienes que seguir los **términos de servicio** (https://discord.com/tos) y las **Pautas de la comunidad de Discord** (https://discord.com/guidelines).`,
+        `8. Se puede hablar de cine y literatura, pero no me seas, no hagas spoilers.`,
+        `9. No pidas roles, puedes asignarte roles en ${roleChannel}.`
+    ]
+    const comunidadSWM = [
+        `1. Si haces streams de SWM recuerda asignarte el rol en ${roleChannel}, puedes compartir tu twitch en ${introduceChannel}.`,
+        `2. Si tienes dudas relacionadas con Twitch, puedes preguntar sin problemas en ${mainChannel}.`,
+        `3. Puedes compartir herramientas, técnicas de estudio, webs y salitas de Forest o similar.`,
+        `4. Puedes realizar todas las sugerencias que quieras para personalizar el servidor.`,
+        `5. Podemos configurar alertas para tus streams, siempre que sea SWM.`,
+        `6. Puedes sugerir juegos para Twitch, avisar cuando vas a jugar y compartir clips de juegos.`
+    ]
     messageReference.channel.send(
-        "```prolog 📕 Normas```"
+        "```prolog\n 📕 Normas \n```"
     );
+    normas.forEach( x => {
+        messageReference.channel.send(BasicEmbedMessage(x, firstColor));
+    })
+    messageReference.channel.send(
+        "```fix\n 📙 Comunidad SWM \n```"
+    );
+    comunidadSWM.forEach( x => {
+        messageReference.channel.send(BasicEmbedMessage(x, secondColor));
+    })
 }
 
 
